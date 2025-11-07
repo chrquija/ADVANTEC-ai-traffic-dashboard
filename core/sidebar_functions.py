88774a653000a0a413ee3d6f2faf06f0d62df758
Christopher Quijano <CQuijano@advantec-usa.com>
2025-11-06 16:38:25 -0800

    Update

--- core/sidebar_functions.py
+++ core/sidebar_functions.py
@@ -178,11 +178,10 @@ def load_volume_data():
             "Washington_St_and_Avenue52": "Avenue 52",
             "Washington_St_and_CalleTampico": "Calle Tampico",
             "Washington_St_and_Calle_Tampico": "Calle Tampico",  # Add this line for the underscore version
-            "Washington_St_and_Village_Shopping_Center": "Village Shopping Ctr",
-            "Washington_St_and_Village_Shopping_Ctr": "Village Shopping Ctr",
+            "Washington_St_and_Village_Shopping_Center": "Village Shopping Center",
             # Add this line for the abbreviated version
             "Washington_St_and_Avenue50": "Avenue 50",
-            "Washington_St_and_Sagebrush_Avenue": "Sagebrush Ave",
+            "Washington_St_and_Sagebrush_Avenue": "Sagebrush Avenue",
             "Washington_St_and_Eisenhower": "Eisenhower",
             "Washington_St_and_Avenue48": "Avenue 48",
             "Washington_St_and_Avenue47": "Avenue 47",
